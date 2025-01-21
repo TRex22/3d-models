d82c5adceb5d30ef190a08b50f1c0838aa07a3bc
trex22 <jason.chalom@ncino.com>
2025-01-21 10:26:04 +0200

    Create a script to build all stls and commit new stl files :cop:

---
+++ build_all_stls.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Create build directory
+mkdir -p build_stls
+
+# Find all SCAD files recursively
+find . -name "*.scad" | while read scad_file; do
+  # Create relative path in build directory
+  relative_path=${scad_file#./}
+  stl_file="build_stls/${relative_path%.scad}.stl"
+
+  # Create directory structure
+  mkdir -p "$(dirname "$stl_file")"
+
+  # Convert SCAD to STL
+  echo "Converting: $scad_file -> $stl_file"
+  docker run --rm -v "$(pwd):/work" -w /work openscad/openscad openscad -o "$stl_file" "$scad_file"
+done
