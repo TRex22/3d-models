420c72579fecf746ea467365aad0b3eff453009b
trex22 <contact@jasonchalom.com>
2026-02-12 16:57:16 +0200

    Update configurations :cop:

--- klipper_installer/install.sh
+++ klipper_installer/install.sh
@@ -13,15 +13,29 @@ cd ~/kiauh && ./kiauh.sh
 # Install Pi MCU
 # make menuconfig
 # Set to Linux Process
+# see: linux-process.cfg
 sudo cp ./scripts/klipper-mcu.service /etc/systemd/system/
 sudo systemctl enable klipper-mcu.service
 
 sudo service klipper stop
 make
 make flash
+sudo systemctl start klipper-mcu.service
 sudo service klipper start
 
-sudo usermod -a -G tty pi
+sudo systemctl status klipper-mcu.service
+
+sudo usermod -a -G tty trex22
+
+# Enable SPI and I2C
+# sudo raspi-config
 
 # Clean-up
 sudo apt clean;sudo apt autoclean;sudo apt autoremove
+
+# Upgrading the main mcu with flash
+# See: ender-4.2.7.cfg
+# Disconnect the screen
+
+# Debugging
+tail -200 ~/printer_data/logs/klippy.log
